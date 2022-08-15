be4ea69c3bb4c659f5ca8a3db3505f85f3e925e0
huy-nh <nguyenhoanghuypc@gmail.com>
2022-08-15 22:15:19 +0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import Admin from "./app/component/Admin";
 import { AuthProvinder } from "./app/providers/AuthProvinder";
 import { Container } from "@mui/system";
-import Customer from "./app/component/Customer";
+import Admin from "app/component/admin";
+import Customer from "app/component/customer";
+import { AuthContext } from "app/providers/AuthContext";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <Container fixed>
+      <Button variant="contained">Button</Button>
       <AuthProvinder>
-        <Admin />
-        <Customer />
+        <AuthContext.Consumer>
+          {(auth) => (auth.id ? <Admin /> : <Customer />)}
+        </AuthContext.Consumer>
       </AuthProvinder>
     </Container>
   );
