1ec1a7a67e79b7c55aad99a6b22c054a4f68a098
Huy Nguyen <huy.h.nguyen@logixtek.com>
2022-11-15 17:59:10 +0700

    no message

--- src/pages/user/PageHome.tsx
+++ src/pages/user/PageHome.tsx
@@ -1,13 +1,7 @@
-import { Container } from "@mui/system";
 import PageHeader from "features/layouts/PageHerder";
 
 function PageHome() {
-  return (
-    <Container>
-      <PageHeader title="Dashboard" />
-      Content
-    </Container>
-  );
+  return <PageHeader title="Dashboard" />;
 }
 
 export default PageHome;
