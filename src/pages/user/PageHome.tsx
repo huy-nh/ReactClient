3eb864145efcb9283cf6532008c65360ad281e11
huy-nh <nguyenhoanghuypc@gmail.com>
2022-11-27 23:52:52 +0700

    no message

--- src/pages/user/PageHome.tsx
+++ src/pages/user/PageHome.tsx
@@ -1,7 +1,76 @@
+import { Box, Container, Stack } from "@mui/system";
+import { Grid, Skeleton } from "@mui/material";
+
 import PageHeader from "features/layouts/PageHerder";
 
 function PageHome() {
-  return <PageHeader title="Dashboard" />;
+  const element = <PageHeader title="Dashboard" />;
+  console.log("type of <PageHeader>", typeof element);
+  console.log("type of PageHeader", typeof PageHeader);
+
+  const Item = ({ color, height }: any) => (
+    <Box width={1} height={height || 40} bgcolor={color || "gainsboro"} />
+  );
+
+  const FullRowItem = () => (
+    <Grid xs={12}>
+      <Item color="crimson" />
+    </Grid>
+  );
+
+  return (
+    <Container>
+      <PageHeader title="Dashboard" />
+
+      <GridLayout grids={[4, 8]} spacing={2}>
+        <Item color="seagreen" />
+        <Item />
+
+        <Grid xs={12} item>
+          <Item color="crimson" />
+        </Grid>
+
+        <Item color="seagreen" />
+        <Item />
+        <Item color="seagreen" />
+        <Item color="orange" />
+        {/* <GridLayout grids={[12]} spacing={2}>
+            <Item />
+            <Item />
+            <Item />
+            
+          </GridLayout> */}
+      </GridLayout>
+    </Container>
+  );
 }
+
+const GridLayout = (props: any) => {
+  const defaultParams = { grids: [2, 8], spacing: 2 };
+  const mergeParams = { ...defaultParams, ...props };
+  const { children, grids, spacing } = mergeParams;
+
+  let counter = 0;
+  return (
+    <Box width={1}>
+      <Grid container alignItems="start" spacing={spacing} border="1px solid black">
+        {children?.map((node, index) => {
+          console.log(node);
+
+          const name = node?.type?.render?.name;
+          if (name === "Grid") {
+            return node;
+          }
+
+          return (
+            <Grid xs={grids[counter++ % grids.length]} md={12} item>
+              {node}
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
+};
 
 export default PageHome;
